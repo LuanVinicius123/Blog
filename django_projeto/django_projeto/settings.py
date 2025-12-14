221d9ee2469fb6bc0cd336b90187c7fb741f5594
LuanVinicius123 <92616312+LuanVinicius123@users.noreply.github.com>
2025-12-14 16:37:01 -0300

    settings.py

--- django_projeto/django_projeto/settings.py
+++ django_projeto/django_projeto/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aleluan2010@gmail.com'
-EMAIL_HOST_PASSWORD= 'eczi eptk ilon bpoa' 
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD= 'senha' 
 
 
 
